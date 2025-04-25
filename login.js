a6710c075a22c97c6c1f7c83f5f8024b2309949a
y_kaneko <y_kaneko@nckk.co.jp>
2025-04-25 13:23:19 +0900

    login.js修正

--- login.js
+++ login.js
@@ -1,7 +1,8 @@
-// Amplfyの初期設定
-aws_amplify.Amplify.configure({
+import { Amplify, Auth } from 'https://develop-srv.d1wmev0i8iycrh.amplifyapp.com';
+
+Amplify.configure({
   Auth: {
-    region: 'ap-northeast-1', // 東京リージョン
+    region: 'ap-northeast-1',
     userPoolId: 'ap-northeast-1_Ld53mCmLp',
     userPoolWebClientId: '75uttfu4ovnj51hnm5qd7b3co0',
   }
@@ -16,10 +17,9 @@ $(function () {
     const password = $('#password').val();
 
     try {
-      const user = await aws_amplify.Auth.signIn(username, password);
+      const user = await Auth.signIn(username, password);
       alert('ログイン成功！ようこそ ' + user.username);
-      console.log('ログイン成功:', user);
-      // 必要があればここで画面遷移やトークン保存
+      window.location.href = 'dashboard.html'; // 成功後に遷移
     } catch (err) {
       alert('ログイン失敗: ' + err.message);
       console.error('ログイン失敗:', err);
