56a0061e771c215ccdd53202891d324f58727ded
y_kaneko <y_kaneko@nckk.co.jp>
2025-05-07 17:10:17 +0900

    AmplifyをWebpackでローカルバンドルする機能を追加

--- js/login.js
+++ js/login.js
@@ -2,15 +2,12 @@ $(document).ready(function () {
   const Amplify = window.aws_amplify;
   const Auth = Amplify.Auth;
 
-  console.log('Amplify:', Amplify);
-  console.log('Auth:', Auth);
-
   if (!Amplify || !Auth) {
     console.error('❌ AmplifyまたはAuthが読み込まれていません');
     return;
   }
 
-  Amplify.configure({
+  Amplify.default.configure({
     Auth: {
       region: 'ap-northeast-1',
       userPoolId: 'ap-northeast-1_Ld53mCmLp',
