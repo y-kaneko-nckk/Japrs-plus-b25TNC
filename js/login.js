31ff56a0f3510e90fe79f1121bf6f3e41ac8e511
y_kaneko <y_kaneko@nckk.co.jp>
2025-05-12 11:42:29 +0900

    ログイン機能の修正

--- js/login.js
+++ js/login.js
@@ -1,65 +1,80 @@
-function OnCognitoAuthenticateUser() {
+$(document).ready(function () {
+    // Cognito認証処理
+    function OnCognitoAuthenticateUser() {
+        // ユーザー名とパスワードを取得
+        var username = $("#email").val();
+        var password = $("#password").val();
 
-	var username = document.getElementById("email").value;
-	var password = document.getElementById("password").value;
+        // 認証データを作成
+        var authenticationData = {
+            Username: username,
+            Password: password,
+        };
 
-	var authenticationData = {
-		Username: username,
-		Password: password,
-	};
+        // 認証詳細を作成
+        var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
 
-	var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(
-		authenticationData
-	);
-	var poolData = {
-		UserPoolId: 'ap-northeast-1_MNT0bbnmI', // Your user pool id here
-		ClientId: 'k5olvrm72340o6alds32p0hem', // Your client id here
-	};
-	var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-	var userData = {
-		Username: username,
-		Pool: userPool,
-	};
+        // ユーザープールの設定
+        var poolData = {
+            UserPoolId: 'ap-northeast-1_MNT0bbnmI', // ユーザープールID
+            ClientId: 'k5olvrm72340o6alds32p0hem', // クライアントID
+        };
+        var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-	var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-	cognitoUser.authenticateUser(authenticationDetails, {
-		onSuccess: function(result) {
-            var idToken = result.getIdToken().getJwtToken();          // IDトークン
-			var accessToken = result.getAccessToken().getJwtToken();  // アクセストークン
-            var refreshToken = result.getRefreshToken().getToken();   // 更新トークン
-            
-            console.log("idToken : " + idToken);
-            console.log("accessToken : " + accessToken);
-            console.log("refreshToken : " + refreshToken);
+        // ユーザーデータを作成
+        var userData = {
+            Username: username,
+            Pool: userPool,
+        };
 
-			//POTENTIAL: Region needs to be set if not already set previously elsewhere.
-			AWS.config.region = 'ap-northeast-1';
+        // Cognitoユーザーを作成
+        var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
 
-			AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-				IdentityPoolId: 'ap-northeast-1:1185c121-9b44-4e97-9c05-ba2f190c7654', // your identity pool id here
-				Logins: {
-					// Change the key below according to the specific region your user pool is in.
-					'cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_MNT0bbnmI': result
-						.getIdToken()
-						.getJwtToken(),
-				},
-			});
+        // ユーザー認証を実行
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: function (result) {
+                // 認証成功時の処理
+                var idToken = result.getIdToken().getJwtToken(); // IDトークン
+                var accessToken = result.getAccessToken().getJwtToken(); // アクセストークン
+                var refreshToken = result.getRefreshToken().getToken(); // 更新トークン
 
-			//refreshes credentials using AWS.CognitoIdentity.getCredentialsForIdentity()
-			AWS.config.credentials.refresh(error => {
-				if (error) {
-					console.error(error);
-				} else {
-					// Instantiate aws sdk service objects now that the credentials have been updated.
-					// example: var s3 = new AWS.S3();
-					console.log('Successfully logged!');
-					window.location.href = 'index.html';
-				}
-			});
-		},
+                console.log("idToken : " + idToken);
+                console.log("accessToken : " + accessToken);
+                console.log("refreshToken : " + refreshToken);
 
-		onFailure: function(err) {
-			alert(err.message || JSON.stringify(err));
-		},
-	});
-}
+                // AWSのリージョンを設定
+                AWS.config.region = 'ap-northeast-1';
+
+                // AWSの認証情報を設定
+                AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+                    IdentityPoolId: 'ap-northeast-1:1185c121-9b44-4e97-9c05-ba2f190c7654', // IDプールID
+                    Logins: {
+                        // ユーザープールのログイン情報を設定
+                        'cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_MNT0bbnmI': idToken,
+                    },
+                });
+
+                // 認証情報を更新
+                AWS.config.credentials.refresh(function (error) {
+                    if (error) {
+                        // 認証情報の更新に失敗した場合
+                        console.error(error);
+                    } else {
+                        // 認証情報の更新に成功した場合
+                        console.log('ログインに成功しました！');
+                        window.location.href = 'index.html'; // ログイン後のリダイレクト
+                    }
+                });
+            },
+            onFailure: function (err) {
+                // 認証失敗時の処理
+                alert(err.message || JSON.stringify(err));
+            },
+        });
+    }
+
+    // ボタンのクリックイベントに認証処理をバインド
+    $("#loginButton").on("click", function () {
+        OnCognitoAuthenticateUser();
+    });
+});
